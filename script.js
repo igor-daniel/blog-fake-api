bf5b92790d9df555d34757dfd4f0d7dafe32bf1e
Igor Daniel da Silva <44319834+igor-daniel@users.noreply.github.com>
2023-02-06 23:45:30 -0300

    html criado.

---
+++ script.js
@@ -0,0 +1,18 @@
+// https://jsonplaceholder.typicode.com/posts
+
+// // function clickCall(){
+// //   fetch('https://jsonplaceholder.typicode.com/posts').then((response)=>{
+// //     return response.json();
+// //   }).then((json)=>{
+// //     console.log(json[0].title)
+// //   })
+// // }
+
+// let clickCall = async ()=>{
+//   let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+//   let json = await response.json();
+//   console.log(json[0].title)
+// }
+
+// let btn = document.querySelector('.btn');
+// btn.addEventListener('click', clickCall )
